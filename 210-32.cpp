eb3c8c0458ab55e5fd373fd72d70410420a633a0
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-11-14 16:19:16 -0800

    created deque and initalized 2 cars

--- 210-32.cpp
+++ 210-32.cpp
@@ -1,12 +1,19 @@
+// COMSC-210 | Lab 32 | Majd Bohsali
+// IDE used: Visual Studio Code
 #include <iostream>
-#include <cstdlib>
 #include <ctime> 
-#include <deque>
+#include <deque> 
 #include "Car.h"
 using namespace std; 
 
-
 int main() {
-    
+    deque<Car> boothLine; 
+
+    Car car1, car2; 
+    boothLine.push_back(car1); 
+    boothLine.push_back(car2);
+
+    cout << "Inital queue:"; 
+
     return 0; 
 }
