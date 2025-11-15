e5e21ff954e7b949d853b2c1fdf7855e0419ac30
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-11-14 16:33:25 -0800

    writting milestone2

--- 210-32.cpp
+++ 210-32.cpp
@@ -2,11 +2,15 @@
 // IDE used: Visual Studio Code
 #include <iostream>
 #include <ctime> 
+#include <cstdlib>
 #include <deque> 
 #include "Car.h"
 using namespace std; 
 
+void printDeque(deque<Car> boothLine);
+
 int main() {
+    srand(0); 
     deque<Car> boothLine; 
 
     Car car1, car2; 
@@ -14,10 +18,40 @@ int main() {
     boothLine.push_back(car2);
 
     cout << "Inital queue:" << endl; 
-    for(int i =0; i < boothLine.size(); i++) { 
-        cout << "    "; 
-        boothLine[i].print(); 
+    printDeque(boothLine); 
+    cout << endl;
+
+    int timeCount = 1; 
+    while(!boothLine.empty()) { 
+        int prop = rand() % 100 + 1; 
+        cout << "Time: " << timeCount << " Operation: "; 
+        if(prop <= 55) { 
+            cout << "Car paid: "; 
+            boothLine.front().print(); 
+            boothLine.pop_front();
+        } else {
+            Car newCar; 
+            boothLine.push_back(newCar);
+            cout << "Joined lane: ";
+            boothLine.back().print();  
+        }
+
+        cout << "Queue" << endl; 
+        printDeque(boothLine); 
+        cout << endl;
+        timeCount++; 
     }
 
     return 0; 
+}
+
+void printDeque(deque<Car> boothLine) { 
+    if(boothLine.empty()) { 
+        cout << "    Empty"; 
+    } else { 
+        for(int i =0; i < boothLine.size(); i++) { 
+            cout << "    "; 
+            boothLine[i].print(); 
+        }
+    }
 }
